f9710091f5fb7f0d6bca4194e16d47760567a790
eojine94 <eojine294@gmail.com>
2022-02-11 23:37:09 +0900

    Add: redux-persist 라이브러리 설치 통해 데이터 localStorage에 저장 기능 구현

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,17 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import login from './login';
 import messenger from './messenger';
 import modals from './modals';
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['login', 'messenger', 'modals'],
+};
+
 const rootReducer = combineReducers({ login, messenger, modals });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
